8bdfa5a553302b3e2aacd78a1b881d9e11b3f87b
accela673 <sardar.kasmaliev@alatoo.edu.kg>
2025-06-12 06:24:34 +0600

    finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,31 @@
 import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 const Home = () => {
-  useEffect(() => {
-    checkSession(); // Проверка сессии при рендере компонента
-  }, []); // Пустой массив зависимостей, чтобы запускалось только при первом рендере
+  const { t, i18n } = useTranslation();
 
   const checkSession = () => {
     const loginTime = localStorage.getItem("loginTime");
+    if (!loginTime) return;
 
-    if (loginTime) {
-      const currentTime = Date.now();
-      const timeElapsed = currentTime - Number(loginTime);
+    const currentTime = Date.now();
+    const timeElapsed = currentTime - Number(loginTime);
+    const twentyFourHours = 24 * 60 * 60 * 1000;
 
-      const twentyFourHours = 24 * 60 * 60 * 1000; // 24 часа в миллисекундах
-
-      if (timeElapsed > twentyFourHours) {
-        // Если прошло больше 24 часов, удаляем данные и выходим
-        localStorage.clear();
-        window.location.href = "/login"; // Перенаправляем на страницу логина
-      }
+    if (timeElapsed > twentyFourHours) {
+      localStorage.clear();
+      window.location.href = "/login";
     }
   };
 
+  useEffect(() => {
+    checkSession();
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold">Главная страница</h1>
-      <p className="text-gray-600 mt-2">Добро пожаловать в Chekpoint.kg!</p>
+      <h1 className="text-3xl font-bold">{t("home.title")}</h1>
+      <p className="text-gray-600 mt-2">{t("home.welcome")}</p>
     </div>
   );
 };
