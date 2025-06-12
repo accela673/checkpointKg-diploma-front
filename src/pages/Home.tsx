508ec452c2328af0b4d7c7047c44b46247fbf239
accela673 <sardar.kasmaliev@alatoo.edu.kg>
2025-06-12 06:25:45 +0600

    finished 2x

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 const Home = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   const checkSession = () => {
     const loginTime = localStorage.getItem("loginTime");
